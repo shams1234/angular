8eb781c26b919415a90b06ac715672618aa74e4c
Anton Shamseev <Anton Shamseev>
2016-04-11 20:27:25 +0300

    just first commit

---
+++ generate.js
@@ -0,0 +1,14 @@
+module.exports = function(){
+    var faker = require("./faker");
+    var _ = require("./lodash");
+    return {
+        products: _.times(6, function(n){
+            return {
+                id: n,
+                description: faker.lorem.text(),
+                title: faker.lorem.word(),
+                img: faker.image.technics()
+            }
+        } )
+    }
+};
