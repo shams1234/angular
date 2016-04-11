1f8df182395c65f39ff5d163ef88b97031ae4402
Anton <aka.shams@gmail.com>
2016-04-12 00:55:02 +0300

    equal height in block with css

--- generate.js
+++ generate.js
@@ -5,9 +5,20 @@ module.exports = function () {
         products: _.times(6, function (n) {
             return {
                 id: n,
-                description: faker.lorem.text(),
+                description: faker.lorem.paragraph(),
                 title: faker.lorem.word(),
                 img: faker.image.technics()
+            }
+        }),
+        users: _.times(3, function(n){
+            return {
+                id: n,
+                firstName: faker.name.findName(),
+                lastName: faker.name.lastName(),
+                userName: faker.internet.userName(),
+                email: faker.internet.email(),
+                phone: faker.phone.phoneNumber()
+
             }
         })
     }
--- js/main.js
+++
@@ -1,5 +0,0 @@
-
-$("p.description").text(function (_, text) {
-    var maxlength = 60;
-    return $.trim(text).substring(0, maxlength);
-});
