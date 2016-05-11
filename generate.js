048ef00c55dd52b081d41f7a5dcd53ec51d21942
Anton <aka.shams@gmail.com>
2016-05-11 14:35:59 +0300

    added new Directive addReview

--- generate.js
+++ generate.js
@@ -12,31 +12,19 @@ module.exports = function () {
                 bestRating: faker.random.number({min:3,max:5}),
                 worstRating: faker.random.number({min : 0, max:3}),
                 img: faker.random.image(),
-                reviews: _.times(faker.random.number(15), function(n){
-                    return {
-                        id: n,
-                        productId: "2",
-                        description : faker.lorem.paragraph(),
-                        author: faker.internet.userName(),
-                        datePublished: faker.date.recent(),
-                        title: faker.name.title(),
-                        ratingValue: faker.random.number(5)
-                    }
-                })
+
         }
         }),
-       
-        users: _.times(15, function(n){
+
+        reviews: _.times(45, function(n){
             return {
                 id: n,
-                productsId : faker.random.number({min:0,max:15}),
-                firstName: faker.name.findName(),
-                lastName: faker.name.lastName(),
-                userName: faker.internet.userName(),
-                userEmail: faker.internet.email(),
-                userPhone: faker.phone.phoneNumber(),
-                userPassword: faker.internet.password()
-
+                productId: faker.random.number(5),
+                description : faker.lorem.paragraph(),
+                author: faker.internet.userName(),
+                datePublished: faker.date.recent(),
+                title: faker.name.title(),
+                ratingValue: faker.random.number(5)
             }
         })
     }
